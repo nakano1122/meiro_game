d0d362840aecb695f071d5745d38f4a04d6d4844
nakano1122 <akito2998@gmail.com>
2024-04-04 14:39:44 +0900

    ElementUI Plus Installation Complete

--- frontend/src/views/AppTop.vue
+++ frontend/src/views/AppTop.vue
@@ -6,7 +6,13 @@ import { RouterLink } from 'vue-router';
 <template>
   <h1>迷路ゲーム！！</h1>
   <h2>（挑戦者の名前）の挑戦</h2>
-  <RouterLink v-bind:to="{name: 'NoviceMode'}">初級</RouterLink>
-  <RouterLink v-bind:to="{name: 'IntermediateMode'}">中級</RouterLink>
-  <RouterLink v-bind:to="{name: 'AdvanceMode'}">上級</RouterLink>
+  <el-button type="success" plain>
+    <RouterLink v-bind:to="{name: 'NoviceMode'}">初級</RouterLink>
+  </el-button>
+  <el-button type="success" plain>
+    <RouterLink v-bind:to="{name: 'IntermediateMode'}">中級</RouterLink>
+  </el-button>
+  <el-button type="success" plain>
+    <RouterLink v-bind:to="{name: 'AdvanceMode'}">上級</RouterLink>
+  </el-button>
 </template>
