9ff5cae07cf759f845707915ffebef56299a7462
nakano1122 <akito2998@gmail.com>
2024-04-06 16:51:43 +0900

    Changing of the screen transition method

--- frontend/src/views/AdvancedMode.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>上級の画面です。</h1>
-</template>
--- frontend/src/views/AppTop.vue
+++ frontend/src/views/AppTop.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
-
 </script>
 
 <template>
   <h1>迷路ゲーム！！</h1>
   <h2>（挑戦者の名前）の挑戦</h2>
   <el-button type="success" plain>
-    <RouterLink v-bind:to="{name: 'NoviceMode'}">初級</RouterLink>
+    <RouterLink v-bind:to="{name: 'MazeStart', params: {mode: 'novice'}}">初級</RouterLink>
   </el-button>
   <el-button type="success" plain>
-    <RouterLink v-bind:to="{name: 'IntermediateMode'}">中級</RouterLink>
+    <RouterLink v-bind:to="{name: 'MazeStart', params: {mode: 'intermediate'}}">中級</RouterLink>
   </el-button>
   <el-button type="success" plain>
-    <RouterLink v-bind:to="{name: 'AdvanceMode'}">上級</RouterLink>
+    <RouterLink v-bind:to="{name: 'MazeStart', params: {mode: 'advance'}}">上級</RouterLink>
   </el-button>
 </template>
