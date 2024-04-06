1fd66633ad8b72882d6194045ac03d1c67d95316
nakano1122 <akito2998@gmail.com>
2024-04-06 12:55:41 +0900

    Completion of the novice maze display

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+import random
 from flask_restful import Api, Resource
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
+def generate_maze():
+    board = [[random.choice([True, False]) for _ in range(7)] for _ in range(8)]
+    return board
+
 class NoviceModeGet(Resource):
     def get(self):
-        return "hello";
+        maze = generate_maze()
+        return jsonify(maze)
 
 api = Api(api_bp)
 api.add_resource(NoviceModeGet, '/start/novice')
--- frontend/src/views/NoviceMode.vue
+++ frontend/src/views/NoviceMode.vue
@@ -2,12 +2,14 @@
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
 
-const sampleText = ref('');
-
+const maze = ref([]);
 const getMaze = async () => {
-  const response = await axios.get('/api/start/novice');
-  console.log(response.data);
-  sampleText.value = response.data;
+  try {
+    const response = await axios.get('/api/start/novice');
+    maze.value = response.data;
+  } catch (error) {
+    console.error('APIからの迷路データの取得に失敗しました。', error);
+  }
 };
 
 onMounted(() => {
@@ -16,10 +18,25 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>初級の画面です。</h1>
-  <div>[[ sampleText ]]</div>
+  <h1>コインをなるべく多く集めてゴールにたどり着こう！！</h1>
+  <table>
+    <tr v-for="(row, rowIndex) in maze" v-bind:key="rowIndex">
+      <td v-for="(cell, cellIndex) in row" v-bind:key="cellIndex">
+        <div v-if="rowIndex == 7 && cellIndex == 3">スタート</div>
+        <div v-else-if="rowIndex == 0 && cellIndex == 3">ゴール</div>
+        <img v-else-if="cell" src="/images/coin_image.png" alt="コイン">
+      </td>
+    </tr>
+  </table>
 </template>
 
 <style>
-
+td {
+  border: 1px solid #000;
+  padding: 8px;
+}
+img {
+  width: 50px;
+  height: auto;
+}
 </style>
