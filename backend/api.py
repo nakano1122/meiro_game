c6e50e06ffd1b40b6b6c0025c88967bfe28ca6ac
nakano1122 <akito2998@gmail.com>
2024-04-12 15:49:45 +0900

    Change from flask_restful to Method Dispatching and APIs

--- backend/api.py
+++ backend/api.py
@@ -1,7 +1,6 @@
-from flask import Blueprint, jsonify, request
-import random
-from flask_restful import Api, Resource
+from flask import Blueprint, jsonify, request, views
 from models import add_user
+import random
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -9,43 +8,25 @@ def generate_maze():
     board = [[random.choice([True, False]) for _ in range(7)] for _ in range(8)]
     return board
 
-class NoviceModeGet(Resource):
-    def get(self):
+class MazeModeView(views.MethodView):
+    def get(self, mode):
         maze = generate_maze()
         stepLimit = 20
         return jsonify({
-            "maze": maze,
-            "stepLimit": stepLimit,
-        })
-class IntermediateModeGet(Resource):
-    def get(self):
-        maze = generate_maze()
-        stepLimit = 20
-        return jsonify({
-            "maze": maze,
-            "stepLimit": stepLimit,
-        })
-class AdvanceModeGet(Resource):
-    def get(self):
-        maze = generate_maze()
-        stepLimit = 20
-        return jsonify({
+            "mode": mode,
             "maze": maze,
             "stepLimit": stepLimit,
         })
 
-class AddUser(Resource):
+class AddUserView(views.MethodView):
     def post(self):
         data = request.get_json()
         name = data.get('name')
         if name:
             add_user(name=name)
-            return {'message': 'ユーザー名が正常に登録されました。'}
+            return jsonify({'message': 'ユーザー名が正常に登録されました。'})
         else:
-            return {'error', '登録に失敗しました。'}, 400
+            return jsonify({'error': '登録に失敗しました。'}), 400
 
-api = Api(api_bp)
-api.add_resource(NoviceModeGet, '/start/novice')
-api.add_resource(IntermediateModeGet, '/start/intermediate')
-api.add_resource(AdvanceModeGet, '/start/advance')
-api.add_resource(AddUser, '/addUser')
+api_bp.add_url_rule('/start/<string:mode>', view_func=MazeModeView.as_view('maze_mode'))
+api_bp.add_url_rule('/addUser', view_func=AddUserView.as_view('add_user'))
